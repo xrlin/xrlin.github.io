8610b78cee78d0ac1104b51e802e956078d11316
xrlin <xrlin@m.scnu.edu.cn>
2019-06-23 11:26:48 +0800

    update github-projects page

--- github-projects/src/routes/ProjectList.js
+++ github-projects/src/routes/ProjectList.js
@@ -9,7 +9,15 @@ var ProjectList = React.createClass({
     this.serverRequest = fetch("https://api.github.com/users/xrlin/repos").then(function(response) {
       return response.json();
     }).then((result) => {
-      let repos = result;
+      let repos = result.filter(repo => repo.fork == false).sort((repo1, repo2) => {
+      		if (repo1.stargazers_count > repo2.stargazers_count) {
+			return true;
+		}
+		if (repo1.stargazers_count < repo2.stargazers_count) {
+			return false;
+		}
+		return repo1.updated_at > repo2.updated_at;
+	});
       this.setState({
         repos: repos
       })
